463d4da3059536426e0f829636d9c0511f12bba3
Oleksandr Sheremet <oleksandr.sheremet@vitech.com.ua>
2019-02-21 17:43:41 +0200

    Updated interlacer to use python3

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 Cafe lentique is a tool set for making lenticular and bareer filter 3D and
 animated images.
 
-Currently available tools are:
+Currently available command line tools are:
 
-interlacer.py
+**interlacer.py**
 takes sequence of images and interleaves them.
 Vertical strips.
 
-cropper.py
+**cropper.py**
 takes sequence of alligned images taken by horizontally moved camera,
 calculates gradually increasing offset and cuts each image to match
 the window.
@@ -19,3 +19,14 @@ Future plans:
 * teapot-friendly installation
 * 2D-3D convertor
 * pitch test producer
+
+# Dependencies
+
+**python3**
+
+  virtualenv env -p python3
+  . env/bin/activate.sh
+
+**pillow**
+
+  pip install -r require
--- interlacer.py
+++ interlacer.py
@@ -1,25 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Lenticular interlacer
 # Author: Alexander Sheremet <asheremet@gmail.com>
 # License: public domain
 
-import Image
+from PIL import Image
 from math import *
 
+
+def mix(a, b, proportion):
+    """
+    @param a, b - color components to mix
+    @param proportion - 0-255.
+    @return mix
+    """
+    return (b * proportion + a * (255 - proportion)) // 255
+
+
 def interlace(nlens, images, height=0, width=0):
-    if height==0:
-        height = images[0].size[1]
-
-    if width==0:
-        width = nlens * images[0].size[0]
-
     img_dest = Image.new(images[0].mode, (width, height))
 
     img1 = images[0]
     xx1 = 0
-    for x_dest in xrange(width):
-        lens_pos = float(x_dest) * float(nlens)/float(width)
+    for x_dest in range(width):
+        lens_pos = x_dest * nlens / width
 
         # 1e-5 is a dirty hack to overcome irrational nature
         # of numbers binary presentation.
@@ -29,19 +33,19 @@ def interlace(nlens, images, height=0, width=0):
 
         proportion = int(modf(img_selector)[0]*255)
 
-        for y_dest in xrange(height):
+        for y_dest in range(height):
             yy1 = img1.size[1] * y_dest / height
             yy2 = img2.size[1] * y_dest / height
 
             pix1 = img1.getpixel((xx1, yy1))
             pix2 = img2.getpixel((xx2, yy2))
 
-            pix = tuple(map(lambda a,b: (b*proportion + a*(255-proportion))/255,
+            pix = tuple(map(lambda a,b: mix(a, b, proportion),
                                 pix1, pix2 ))
 
             img_dest.putpixel((x_dest, y_dest), pix)
 
-        print "%d / %d" % (x_dest, width)
+        print ("%d / %d" % (x_dest, width))
         img1 = img2
         xx1 = xx2
 
@@ -51,23 +55,25 @@ def interlace(nlens, images, height=0, width=0):
 if __name__ == '__main__':
     import optparse
 
-    parser = optparse.OptionParser()
+    usage = "Usage: %prog options {list of source images}"
+
+    parser = optparse.OptionParser(usage)
     parser.add_option("-o", dest="ofile",
                       help="write resulting image to FILE", metavar="FILE")
     parser.add_option("-t", dest="height", type='int',
-                      help="height of result in pixels", metavar="HEIGHT")
+                      help="height of resulting image in pixels", metavar="HEIGHT")
     parser.add_option("-w", dest="width", type='int',
-                      help="width of result in pixels", metavar="FILE")
+                      help="width of resulting image in pixels", metavar="FILE")
     parser.add_option("-n", dest="nlens", type='int',
-                      help="number of lenses on top of image", metavar="LENS_NUMBER")
+                      help="number of lenticulas on top of image", metavar="LENS_NUMBER")
     parser.add_option("-r", "--dpi", dest="dpi",
                       help="x and y print resolutions (dpi)", metavar="(x_res,y_res)")
 
     (options, args) = parser.parse_args()
 
-    ims = map(lambda x: Image.open(x), args)
+    images = [Image.open(file_name) for file_name in args]
 
-    dest = interlace(options.nlens, ims, height=options.height, width=options.width)
+    interlaced_image = interlace(options.nlens, images, height=options.height, width=options.width)
 
-    dest.save(options.ofile, dpi=eval(options.dpi))
+    interlaced_image.save(options.ofile, dpi=eval(options.dpi))
 
